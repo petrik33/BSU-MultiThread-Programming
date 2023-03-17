efc20749ef9fd10f4ce63291591225ae93b28d6d
Timofey Petrikevich <petrik3003@gmail.com>
2023-03-17 19:41:54 +0300

    misc: reporter definitions boilerplate

---
+++ ProcessCreation1/Reporter.cpp
@@ -0,0 +1,13 @@
+#include "Reporter.h"
+
+namespace reporter {
+
+DWORD __stdcall reporter::Reporter(LPVOID iReporterProps) { return 0; }
+
+Employee* reporter::readEmployeeFromBinary(std::ifstream& in) {
+  return nullptr;
+}
+
+void reporter::writeReportLine(Employee* employee, double wage) {}
+
+}  // namespace reporter
